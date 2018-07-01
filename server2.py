443ed8dd9ee203f60723042bb4459541d55c1b5c
Dengke Du <pinganddu90@gmail.com>
2018-07-01 16:48:38 +0000

    pass queue between the two work thread
    Because the child tcp thread would use the callback function's data
    in main thread, so we use the Queue to pass data between two work
    thread.
    
    Signed-off-by: Dengke Du <pinganddu90@gmail.com>

--- server2.py
+++ server2.py
@@ -1,28 +1,39 @@
 import os
 import signal
 import socket
-from SocketServer import BaseRequestHandler,ThreadingTCPServer
+from SocketServer import *
 import threading
 import subprocess
-from multiprocessing import Process
+from multiprocessing import Process,Queue
 from megapi import *
+import marshal
 
 def onRead(a):
-    socket_child[0].sendall("ultrasonic:"+str(a))
+    if len(socket_child):
+        socket_child[0].sendall("ultrasonic:"+str(a))
+        ultra=a
+        q.put(str(ultra))
+    s.sendto("ultrasonic"+str(a),('255.255.255.255',12345))
 
 def onGas(a):
-    socket_child[0].sendall("gas:"+str(a))
+    if len(socket_child):
+        socket_child[0].sendall("gas:"+str(a))
+    s.sendto("gas"+str(a),('255.255.255.255',12345))
 
 def onPir(a):
-    socket_child[0].sendall("pir:"+str(a))
+    if len(socket_child):
+        socket_child[0].sendall("pir:"+str(a))
+    s.sendto("pir"+str(a),('255.255.255.255',12345))
 
 socket_child = []
+ultra = 0
 
 class Handler(BaseRequestHandler):
     ProcessListSensor = []
-    def onRead1(a):
-        print str(a)
+    def onRead1(self,a):
+        onRead()
     def handle(self):
+        self.queue = self.server.queue
         socket_child.append(self.request)
         address,pid = self.client_address
         print('%s connected!' % address)
@@ -36,23 +47,23 @@ class Handler(BaseRequestHandler):
                     cmd = data.split(":")[1]
                     if 'left' in cmd:
                         bot.motorRun(1,0)
-                        bot.motorRun(2,30)
+                        bot.motorRun(4,30)
                         print cmd
                     if 'right' in cmd:
                         bot.motorRun(1,30)
-                        bot.motorRun(2,0)
+                        bot.motorRun(4,0)
                         print cmd
                     if 'forward' in cmd:
                         bot.motorRun(1,30)
-                        bot.motorRun(2,30)
+                        bot.motorRun(4,30)
                         print cmd
                     if 'back' in cmd:
                         bot.motorRun(1,-30)
-                        bot.motorRun(2,-30)
+                        bot.motorRun(4,-30)
                         print cmd
                     if 'stop' in cmd:
                         bot.motorRun(1,0)
-                        bot.motorRun(2,0)
+                        bot.motorRun(4,0)
                         print cmd
                 if 'sensor' in data:
                     cmd = data.split(":")[1]
@@ -62,6 +73,7 @@ class Handler(BaseRequestHandler):
                             while 1:
                                 sleep(0.1)
                                 bot.ultrasonicSensorRead(6,onRead)
+                                print self.queue.get()
                         else:
                             self.ProcessListSensor.append(pid)
                             print pid
@@ -92,13 +104,17 @@ class Handler(BaseRequestHandler):
                 break
 
 if __name__  == '__main__':
+    q = Queue()
     bot = MegaPi()
     bot.start('/dev/ttyS0')
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
     BUF_SIZE = 1024
     HOST = ''
     PORT = 8001
     ADDR = (HOST,PORT)
     server = ThreadingTCPServer(ADDR,Handler)
+    server.queue = q
     print 'listening'
     server.serve_forever()
     print(server)
